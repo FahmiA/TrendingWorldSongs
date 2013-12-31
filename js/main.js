ed2b7c226b7f582c2deba977a86c42c73942762f
Fahmi Abdulhamid <findfahmi@gmail.com>
2014-01-01 10:21:18 +1300

    Can select countries

--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
 
-var mapWidth = 960;
-var mapHeight = 480;
+createWorldMap();
 
-var projection = d3.geo.equirectangular()
-    .scale(153)
-    .translate([mapWidth / 2, mapHeight / 2])
-    .precision(0.1);
+function handleCountryDeselect(countryElement) {
+    d3.select(countryElement)
+        .classed('country-selected', false);
+};
 
-var path = d3.geo.path()
-    .projection(projection);
+function handleCountrySelect(countryElement, countryData) {
+    // Display the country as selected
+    d3.select(countryElement)
+        .classed('country-selected', true);
 
+};
 
-var svg = d3.select('#drawingArea');
+function createWorldMap() {
+    // The map dimensions
+    var mapWidth = 960;
+    var mapHeight = 480;
 
-/*var graticule = d3.geo.graticule();
-svg.append('path')
-    .datum(graticule)
-    .attr('class', 'graticule')
-    .attr('d', path);*/
+    // Details of the map projection
+    var projection = d3.geo.equirectangular()
+        .scale(153)
+        .translate([mapWidth / 2, mapHeight / 2])
+        .precision(0.1);
 
-d3.json("data/worldMap.json", function(error, world) {
-  svg.selectAll('path')
-    .data(world.features)
-    .enter()
-    .append('path')
-    .attr('d', path)
-    .attr('class', 'country');
-});
+    var path = d3.geo.path()
+        .projection(projection);
+
+    // Load the map data and draw the map
+    var svg = d3.select('#drawingArea');
+    d3.json("data/worldMap.json", function(error, world) {
+        var prevCountryElement;
+        svg.selectAll('path')
+            .data(world.features)
+            .enter()
+            .append('path')
+            .attr('d', path)
+            .attr('class', 'country')
+            .attr('title', function(d) {
+                return d.properties.name;
+            })
+            .on('mouseup', function(d) {
+                if(prevCountryElement)
+                    handleCountryDeselect(prevCountryElement);
+                handleCountrySelect(this, d);
+                prevCountryElement = this;
+            });
+    });
+}
 
