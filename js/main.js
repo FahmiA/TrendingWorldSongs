49181f5fefdc4a4ec8ac5bbd2cc39d89736999b9
Fahmi Abdulhamid <findfahmi@gmail.com>
2014-01-01 09:50:51 +1300

    Countries can be highlighted.

--- js/main.js
+++ js/main.js
@@ -10,24 +10,21 @@ var projection = d3.geo.equirectangular()
 var path = d3.geo.path()
     .projection(projection);
 
-var graticule = d3.geo.graticule();
 
 var svg = d3.select('#drawingArea');
 
+/*var graticule = d3.geo.graticule();
 svg.append('path')
     .datum(graticule)
     .attr('class', 'graticule')
-    .attr('d', path);
+    .attr('d', path);*/
 
-d3.json("js/lib/world-50m.json", function(error, world) {
-  svg.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
-      .attr("class", "land")
-      .attr("d", path);
-
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);
+d3.json("data/worldMap.json", function(error, world) {
+  svg.selectAll('path')
+    .data(world.features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('class', 'country');
 });
 
