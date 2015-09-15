93fc54d4521578017b1674494182b6be3669d816
Fahmi Abdulhamid <findfahmi@gmail.com>
2015-09-15 20:42:16 +1200

    Add script to automate transform of SVGs

---
+++ data/fixMarinasMess.py
@@ -0,0 +1,67 @@
+import sys
+
+import xml.etree.ElementTree as ET
+import re
+
+def appendStyleElement(svgRoot):
+    print 'Appending CSS style'
+
+    svgRoot.set('xmlns:xhtml', 'http://www.w3.org/1999/xhtml')
+
+    defsElement = ET.Element('defs')
+
+    linkElement = ET.Element('xhtml:link', {
+        'href': 'style.css',
+        'type': 'text/css',
+        'rel': 'stylesheet'
+    })
+
+    svgRoot.insert(0, defsElement)
+    defsElement.append(linkElement)
+
+def resolveDulicateScribbleIds(svgRoot):
+    print 'Resolving scribble country IDs'
+
+    scribblesElement = None
+    for childElement in svgRoot:
+        if(childElement.tag == '{http://www.w3.org/2000/svg}g' and childElement.get('id') == 'scribbles'):
+            scribblesElement = childElement
+            break
+
+    if scribblesElement == None:
+        print 'WARN: Could not find "scribbles" group'
+        return
+
+    for countryElement in scribblesElement:
+        elementID = countryElement.get('id')
+        elementID = re.sub(r'_\d+_$', '', elementID) # Remove _1_, _2_, and the likes
+        elementID = 'Scibble-'+elementID
+        countryElement.set('id', elementID)
+
+def updateViewbox(svgRoot):
+    print 'Correcting SVG ViewBox'
+
+    svgRoot.set('viewBox', '250 200 200 400')
+
+if __name__ == '__main__':
+    inputPath = sys.argv[1]
+    outputPath = 'out.svg'
+
+    doc = ET.parse(inputPath)
+    svgRoot = doc.getroot();
+    print svgRoot.tag
+
+    appendStyleElement(svgRoot)
+    resolveDulicateScribbleIds(svgRoot)
+    updateViewbox(svgRoot)
+
+    print 'Writing file to', outputPath
+
+    ET.register_namespace('','http://www.w3.org/2000/svg')
+    doc.write(outputPath,
+             xml_declaration = True,
+             encoding = 'utf-8',
+             method = 'xml')
+
+    print 'Done'
+
